69a9cdbfa11bd796ed7120f41ee622af230da54e
jicaifeng <jicaifeng@supremind.com>
2022-09-20 19:54:31 +0800

    处理一些控制台的警告

--- react-app-qiankun-sub/src/components/HelloModal.jsx
+++ react-app-qiankun-sub/src/components/HelloModal.jsx
@@ -6,7 +6,7 @@ export default function() {
   return (
     <>
       <Button onClick={() => setVisible(true)}>CLICK ME</Button>
-      <Modal visible={visible} onOk={() => setVisible(false)} onCancel={() => setVisible(false)} title="qiankun">
+      <Modal open={visible} onOk={() => setVisible(false)} onCancel={() => setVisible(false)} title="qiankun">
         Probably the most complete micro-frontends solution you ever met
       </Modal>
     </>
